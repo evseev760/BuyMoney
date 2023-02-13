79f03949a84072bbb3ac4cb39594c58e38eb1377
PEvseev <pavel.evseev@firstlinesoftware.com>
2023-02-13 19:28:15 +0300

    added cros

--- express/index.js
+++ express/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const authRouter = require("./authRouter");
 const PORT = process.env.PORT || 5000;
 
