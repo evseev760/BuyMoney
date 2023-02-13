241bdbc43e222cb4c20ecc2f25d92473c42f98fe
PEvseev <pavel.evseev@firstlinesoftware.com>
2023-02-13 19:25:41 +0300

    added cros

--- express/index.js
+++ express/index.js
@@ -1,21 +1,23 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const authRouter = require('./authRouter')
-const PORT = process.env.PORT || 5000
+const express = require("express");
+const mongoose = require("mongoose");
+const authRouter = require("./authRouter");
+const PORT = process.env.PORT || 5000;
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use("/auth", authRouter)
+app.use(cors());
+app.use(express.json());
+app.use("/auth", authRouter);
 
 const start = async () => {
-    try {
-        await mongoose.connect(`mongodb+srv://admin:admin123@cluster0.rx50cdv.mongodb.net/test?retryWrites=true&w=majority`)
-        app.listen(PORT, () => console.log(`server started on port ${PORT}`))
-    } catch (e) {
-        console.log(e)
-    }
-}
+  try {
+    await mongoose.connect(
+      `mongodb+srv://admin:admin123@cluster0.rx50cdv.mongodb.net/test?retryWrites=true&w=majority`
+    );
+    app.listen(PORT, () => console.log(`server started on port ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+};
 
-start()
-
+start();
