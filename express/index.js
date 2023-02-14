893f1908e1f9a5e056870811230ee55d04473450
PEvseev <pavel.evseev@firstlinesoftware.com>
2023-02-14 15:30:53 +0300

    added client chain

--- express/index.js
+++ express/index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const config = require("config");
 const authRouter = require("./authRouter");
+const path = require("path");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
@@ -16,6 +17,12 @@ const start = async () => {
     await mongoose.connect(
       `mongodb+srv://admin:admin123@cluster0.rx50cdv.mongodb.net/test?retryWrites=true&w=majority`
     );
+    app.use(express.static(__dirname));
+    app.use(express.static(path.resolve(__dirname, "../client/build")));
+
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+    });
     app.listen(PORT, () => console.log(`server started on port ${PORT}`));
   } catch (e) {
     console.log(e);
