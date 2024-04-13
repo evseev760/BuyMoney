e12e5758ca01f9b18bcec6ba7e678b5973a277bb
evseev760 <pavel.evseev.94@gmail.com>
2024-04-13 14:53:24 +0700

    Update index.js

--- express/index.js
+++ express/index.js
@@ -3,19 +3,15 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
 const config = require("config");
+const fs = require("fs");
+const https = require("https");
 const PORT = process.env.PORT || config.get("PORT");
+const { Server } = require("socket.io");
 
 const authRouter = require("./sections/auth/authRouter");
 const chatRouter = require("./sections/chat/chatRouter");
 
 const app = express();
-
-const { createServer } = require("http");
-const { Server } = require("socket.io");
-const httpServer = createServer(app);
-const io = new Server(httpServer, {
-  /* options */
-});
 
 app.use(cors());
 app.use(express.json());
@@ -31,20 +27,43 @@ const start = async () => {
     app.get("*", (req, res) => {
       res.sendFile(path.join(__dirname, "../client/build", "index.html"));
     });
-    app.listen(PORT, () => console.log(`server started on port ${PORT}`));
+
+    const privateKey = fs.readFileSync("/etc/ssl/private/privatekey.pem", "utf8");
+    const certificate = fs.readFileSync("/etc/ssl/certs/pocketmoneytgru.pem", "utf8");
+    const credentials = { key: privateKey, cert: certificate };
+
+    const httpsServer = https.createServer(credentials, app);
+
+     const io = new Server(httpsServer);
+
+    io.on("connection", (socket) => {
+      socket.on("addMessage", (data) => {
+        io.emit("addMessage", data);
+      });
+      socket.on("chatCreated", (data) => {
+        io.emit("chatCreated", data);
+      });
+    });
+    
+    httpsServer.listen(PORT, () => {
+      console.log(`HTTPS Server started on port ${PORT}`);
+    });
   } catch (e) {
     console.log(e);
   }
 };
 
-io.on("connection", (socked) => {
-  socked.on("addMessage", (data) => {
-    io.emit("addMessage", data);
-  });
-  socked.on("chatCreated", (data) => {
-    io.emit("chatCreated", data);
-  });
-});
+// const { Server } = require("socket.io");
+// const httpsServer = https.createServer(credentials, app);
+// const io = new Server(httpsServer);
+
+// io.on("connection", (socket) => {
+//   socket.on("addMessage", (data) => {
+//     io.emit("addMessage", data);
+//   });
+//   socket.on("chatCreated", (data) => {
+//     io.emit("chatCreated", data);
+//   });
+// });
 
 start();
-httpServer.listen(config.get("WS_PORT"));
