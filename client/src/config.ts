3cee477b8b5797af96563411391861fa34f9b794
evseev760 <pavel.evseev.94@gmail.com>
2024-04-13 15:39:19 +0700

    Update config.ts

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,4 +1,4 @@
-export const API_URL = `https://pocketmoneytgru.ru/`;
-export const WS_URL = `wss://pocketmoneytgru.ru:3002/`;
+export const API_URL = `https://pocketmoneytg.ru/`;
+export const WS_URL = `wss://pocketmoneytg.ru:3002/`;
 // export const API_URL = `http://localhost:3000/`;
 // export const WS_URL = `http://localhost:3002/`;
