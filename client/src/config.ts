716836d2fe430f02d66c4948864757ba725fe1ff
evseev760 <pavel.evseev.94@gmail.com>
2024-04-13 15:02:23 +0700

    Update config.ts

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,4 +1,4 @@
-export const API_URL = `http://185.211.170.127:443/`;
-export const WS_URL = `http://185.211.170.127:3002/`;
+export const API_URL = `https://185.211.170.127:443/`;
+export const WS_URL = `wss://185.211.170.127:3002/`;
 // export const API_URL = `http://localhost:3000/`;
 // export const WS_URL = `http://localhost:3002/`;
