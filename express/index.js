db4d7b73992fdbf131c36b1801721d39c9417ce1
pevseev <pevseev@netevia.com>
2024-06-05 16:19:08 +0700

    add port_prod

--- express/index.js
+++ express/index.js
@@ -3,15 +3,10 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
 const config = require("config");
-const cronJobs = require("./cronJobs");
-const fs = require("fs");
-const https = require("https");
-const { createServer } = require("http");
 const compression = require("compression");
+const { createServer } = require("http");
 
 const { initializeSocket } = require("./socketHandler");
-
-const PORT = config.get("PORT");
 
 const authRouter = require("./sections/auth/authRouter");
 const offerRouter = require("./sections/offer/offerRouter");
@@ -24,60 +19,81 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(compression());
-app.use("/auth", authRouter);
-app.use("/offer", offerRouter);
-app.use("/verify", verificationRouter);
-app.use("/currency", currencyApiRouter);
-app.use("/application", applicationApiRouter);
+
+app.use("/api/auth", authRouter);
+app.use("/api/offer", offerRouter);
+app.use("/api/verify", verificationRouter);
+app.use("/api/currency", currencyApiRouter);
+app.use("/api/application", applicationApiRouter);
 
 const isProduction = process.env.NODE_ENV === "production";
 
 let server;
 let dbUrl;
+let PORT;
+
 if (isProduction) {
-  const privateKey = fs.readFileSync("/etc/ssl/private/privatekey.pem", "utf8");
-  const certificate = fs.readFileSync(
-    "/etc/ssl/certs/pocketmoneytgru.pem",
-    "utf8"
-  );
-  const credentials = { key: privateKey, cert: certificate };
-  server = https.createServer(credentials, app);
   dbUrl = config.get("dbUrlProd");
+  PORT = config.get("PORT_PROD");
 } else {
   dbUrl = config.get("dbUrl");
-  server = createServer(app);
+  PORT = config.get("PORT");
+
+  // В dev среде, обрабатываем статические файлы через express.static
+  app.use(express.static(path.resolve(__dirname, "../client/build")));
+
+  app.get("*", (req, res) => {
+    const filePath = path.resolve(__dirname, "../client/build", "index.html");
+    fs.stat(filePath, (err) => {
+      if (err) {
+        if (err.code === "ENOENT") {
+          res
+            .status(404)
+            .sendFile(path.join(__dirname, "./public", "notFound.html"));
+        } else {
+          res.status(500).send("Internal Server Error");
+        }
+      } else {
+        res.sendFile(filePath);
+      }
+    });
+  });
+
+  app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, "./public", "notFound.html"));
+  });
 }
 
 const start = async () => {
   try {
-    await mongoose
-      .connect(dbUrl, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-        serverSelectionTimeoutMS: 5000,
-        socketTimeoutMS: 45000,
-      })
-      .then(() => console.log("MongoDB connected"))
-      .catch((err) => console.error("MongoDB connection error:", err));
-
-    app.use(express.static(path.resolve(__dirname, "../client/build")));
-
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+    await mongoose.connect(dbUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      serverSelectionTimeoutMS: 5000,
+      socketTimeoutMS: 45000,
     });
-    app.use((req, res, next) => {
-      res
-        .status(404)
-        .sendFile(path.join(__dirname, "./public", "notFound.html"));
-    });
+    console.log("MongoDB connected");
 
-    server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+    server = createServer(app);
+    server.listen(PORT, () =>
+      console.log(
+        `Server started on port ${PORT} in ${
+          isProduction ? "production" : "development"
+        } mode`
+      )
+    );
 
     initializeSocket(server);
   } catch (e) {
-    console.log(e);
+    console.error("Error starting server:", e);
+
+    app.use((req, res, next) => {
+      res
+        .status(500)
+        .sendFile(path.join(__dirname, "./public", "notFound.html"));
+    });
   }
 };
 
